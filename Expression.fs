9119525884c2b08edb347b99f384efec2dd7fb81
Walter Tetzner <walter@waltertetzner.net>
2011-06-18 14:39:08 -0400

    Added Expression.fs, and updated the namespace name of TranslateExpression.

---
+++ Expression.fs
@@ -0,0 +1,45 @@
+﻿namespace org.bovinegenius.DataBeast.Expression
+open System.Linq.Expressions
+
+module Match =
+  let (|BinaryExpression|_|) (e:Expression) =
+    if e :? BinaryExpression
+      then let bin = e :?> BinaryExpression
+             in Some (bin.Left, bin.Right)
+      else None
+
+  let (|Equal|_|) (e:Expression) =
+    if e.NodeType = ExpressionType.Equal
+      then match e with
+            | BinaryExpression (l, r) -> Some (l, r)
+            | _ -> None
+      else None
+     
+  let (|LessThan|_|) (e:Expression) =
+    if e.NodeType = ExpressionType.LessThan
+      then match e with
+            | BinaryExpression (l, r) -> Some (l, r)
+            | _ -> None
+      else None
+
+  let (|GreaterThen|_|) (e:Expression) =
+    if e.NodeType = ExpressionType.GreaterThan
+      then match e with
+            | BinaryExpression (l, r) -> Some (l, r)
+            | _ -> None
+       else None
+
+  let (|LessOrEqual|_|) (e:Expression) =
+    if e.NodeType = ExpressionType.LessThanOrEqual
+      then match e with
+            | BinaryExpression (l, r) -> Some (l, r)
+            | _ -> None
+      else None
+
+  let (|GreaterOrEqual|_|) (e:Expression) =
+    if e.NodeType = ExpressionType.GreaterThanOrEqual
+      then match e with
+            | BinaryExpression (l, r) -> Some (l, r)
+            | _ -> None
+      else None
+
--- TranslateExpression.fs
+++ TranslateExpression.fs
@@ -1,8 +1,8 @@
-﻿namespace org.bovinegenius.DataBeast
+﻿namespace org.bovinegenius.DataBeast.Expression
 open org.bovinegenius.DataBeast
 open System.Linq.Expressions
 
-module TranslateExpression =
+module Translate =
   let translate_to_sql (dbms : Dbms) (expression : Expression) = ""
 
 
