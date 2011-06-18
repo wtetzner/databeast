9973579ec960e6949d45169259b52fb3eac5b89b
Walter Tetzner <walter@waltertetzner.net>
2011-06-18 13:54:11 -0400

    Initial Commit

---
+++ Database.fs
@@ -0,0 +1,7 @@
+﻿namespace org.bovinegenius.DataBeast
+
+type Dbms = 
+   | SqlServer = 1u
+   | MySql = 2u
+   | PostgreSql = 3u
+
---
+++ TranslateExpression.fs
@@ -0,0 +1,8 @@
+﻿namespace org.bovinegenius.DataBeast
+open org.bovinegenius.DataBeast
+open System.Linq.Expressions
+
+module TranslateExpression =
+  let translate_to_sql (dbms : Dbms) (expression : Expression) = ""
+
+
