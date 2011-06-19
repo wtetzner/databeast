8a1d5e0688c2ba3fc9ec4a521ac48a353a50d2af
Walter Tetzner <walter@waltertetzner.net>
2011-06-19 00:22:46 -0400

    Added new active patterns for matching Expressions, and updated the expression printer.

--- MatchExpression.fs
+++ MatchExpression.fs
@@ -1,66 +1,67 @@
 ﻿namespace org.bovinegenius.DataBeast.Expression
 open System.Linq.Expressions
 open System.Reflection;
+open System
 
 module Match =
-  let (|BinaryExpression|_|) (e:Expression) =
+  let (|Binary|_|) (e:Expression) =
     if e :? BinaryExpression
       then let bin = e :?> BinaryExpression
-             in Some (bin.Left, bin.Right)
+             in Some (bin.NodeType, bin.Left, bin.Right)
       else None
 
   let (|Equal|_|) (e:Expression) =
     if e.NodeType = ExpressionType.Equal
       then match e with
-            | BinaryExpression (l, r) -> Some (l, r)
+            | Binary (t, l, r) -> Some (l, r)
             | _ -> None
       else None
      
   let (|LessThan|_|) (e:Expression) =
     if e.NodeType = ExpressionType.LessThan
       then match e with
-            | BinaryExpression (l, r) -> Some (l, r)
+            | Binary (t, l, r) -> Some (l, r)
             | _ -> None
       else None
 
   let (|GreaterThen|_|) (e:Expression) =
     if e.NodeType = ExpressionType.GreaterThan
       then match e with
-            | BinaryExpression (l, r) -> Some (l, r)
+            | Binary (t, l, r) -> Some (l, r)
             | _ -> None
        else None
 
   let (|LessOrEqual|_|) (e:Expression) =
     if e.NodeType = ExpressionType.LessThanOrEqual
       then match e with
-            | BinaryExpression (l, r) -> Some (l, r)
+            | Binary (t, l, r) -> Some (l, r)
             | _ -> None
       else None
 
   let (|GreaterOrEqual|_|) (e:Expression) =
     if e.NodeType = ExpressionType.GreaterThanOrEqual
       then match e with
-            | BinaryExpression (l, r) -> Some (l, r)
+            | Binary (t, l, r) -> Some (l, r)
             | _ -> None
       else None
 
   let (|NotEqual|_|) (e:Expression) = 
     if e.NodeType = ExpressionType.NotEqual
       then match e with
-            | BinaryExpression (l, r) -> Some (l, r)
+            | Binary (t, l, r) -> Some (l, r)
             | _ -> None
       else None
 
   let (|UnaryExpression|_|) (e:Expression) =
     if e :? UnaryExpression
       then let un = e :?> UnaryExpression
-             in Some (un.Operand)
+             in Some (un.NodeType, un.Operand)
       else None
 
   let (|Not|_|) (e:Expression) =
     if e.NodeType = ExpressionType.Not
       then match e with
-            | UnaryExpression (o) -> Some (o)
+            | UnaryExpression (t, o) -> Some (t, o)
             | _ -> None
       else None
 
@@ -129,29 +130,51 @@ module Match =
   let (|AndAlso|_|) (e:Expression) =
     if e.NodeType = ExpressionType.AndAlso
       then match e with
-            | BinaryExpression (l, r) -> Some (l, r)
+            | Binary (t, l, r) -> Some (l, r)
             | _ -> None
       else None
 
   let (|OrElse|_|) (e:Expression) =
     if e.NodeType = ExpressionType.OrElse
       then match e with
-            | BinaryExpression (l, r) -> Some (l, r)
+            | Binary (t, l, r) -> Some (l, r)
             | _ -> None
       else None
 
   let (|Parameter|_|) (e:Expression) =
-    match e with
-     | MemberAccess (n, e, m) ->
-         if e.NodeType = ExpressionType.Parameter
-           then let par = e :?> ParameterExpression
-                  in Some (par.Name, par.Type)
-           else None
-     | _ -> None
+    if e.NodeType = ExpressionType.Parameter
+      then let par = e :?> ParameterExpression
+             in Some (par.Name, par.Type)
+      else None
 
   let (|Index|_|) (e:Expression) =
     match e with
      | MethodCall ("get_Item", m, o, a) -> Some (o, a.Item 0)
      | _ -> None
 
-     
+  let (|Lambda|_|) (e:Expression) =
+    if e :? LambdaExpression
+      then let l = e :?> LambdaExpression
+             in Some (l.Parameters, l.Body)
+      else None
+
+  let (|FreeVariable|_|) (e:Expression) =
+    if e :? MemberExpression
+      then let m = e :?> MemberExpression
+             in if m.NodeType <> ExpressionType.Parameter
+                  then Some (m.Member.Name, m.Expression, m.Member)
+                  else None
+      else None
+
+  let (|StringConstant|_|) (e:Expression) =
+    match e with
+     | Constant (tname, t, o) -> if o :? String
+                                   then Some (tname, t, o)
+                                   else None
+     | _ -> None
+
+  let (|New|_|) (e:Expression) =
+    if e :? NewExpression
+      then let n = e :?> NewExpression
+             in Some (n.Constructor, n.Arguments, n.Members)
+      else None
--- PrintExpression.fs
+++ PrintExpression.fs
@@ -1,12 +1,30 @@
 ﻿namespace org.bovinegenius.DataBeast
+open System.Collections.Generic
+open System.Collections
 open System.Linq.Expressions
+open System.Reflection
+open System.Linq
 open System
 open org.bovinegenius.DataBeast.Expression.Match
 
 module PrintExpression =
-  let print_indented (e:Expression) level =
+
+  let rec print_exps (exps:List<Expression>) =
+    String.Join (", ", (exps.Select (fun x -> print_exp x)).ToArray())
+
+  and print_exp (e:Expression) =
     match e with
-     | Constant (tn, o, t) -> String.Format ("{0}(Constant '{1}'", level, tn)
-     | _ -> failwith "Don't know expression '{0}'" e.ToString
+     | StringConstant (tname, t, o) -> String.Format ("\"{0}\"", o.ToString().Replace("\"", "\\\""))
+     | Constant (typeName, t, o) -> String.Format ("(Constant:{0} {1})", typeName, o.ToString())
+     | Index (o, arg) -> String.Format("{0}[{1}]", o.ToString(), print_exp arg)
+     | FreeVariable (name, o, mem) -> String.Format ("{0}", name)
+     | MethodCall (name, m, o, args) -> String.Format ("{1}.{0}({2}))", name, (if o = null then m.DeclaringType.Name else (o.ToString())), print_exps (args.ToList()))
+     | Binary (t, l, r) -> String.Format ("{1} {0} {2}", t.ToString(), print_exp l, print_exp r)
+     | UnaryExpression (t, o) -> String.Format ("{0}({1})", t.ToString(), print_exp o)
+     | Lambda (args, body) -> String.Format ("(fun ({0}) -> {1})", print_exps (args.Cast<Expression>().ToList()), print_exp body)
+     | Parameter (name, t) -> String.Format ("{0}", name, t.ToString())
+     | MemberAccess (name, o, mem)  -> String.Format ("{0}.{1}", print_exp o, name)
+     | New (cons, args, mem) -> String.Format ("new {0} {{ {1} }}", cons.DeclaringType.Name,  String.Join (", ", (Array.map2 (fun arg (mem:MemberInfo) -> String.Format ("{0} = {1}", mem.Name, print_exp arg)) (args.ToArray()) (mem.ToArray()))))
+     | _ -> failwith (String.Format ("Don't know expression '{0}' of type '{1}'", e.ToString(), e.NodeType.ToString()))
 
 
